f807b3a4b47ea0da05c50a4aa1f29266a001324f
Nannan Gu <396426364@qq.com>
2019-02-25 20:49:18 +1100

    new

--- eureka/src/test/java/com/gnn/eureka/EurekaApplicationTests.java
+++ eureka/src/test/java/com/gnn/eureka/EurekaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.gnn.advertisement;
+package com.gnn.eureka;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class AdvertisementApplicationTests {
+public class EurekaApplicationTests {
 
     @Test
     public void contextLoads() {
