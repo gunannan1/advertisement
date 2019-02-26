890c85be0704caf5f64461a182e52941424b72c3
Nannan Gu <396426364@qq.com>
2019-02-26 13:23:26 +1100

    new

--- eureka/src/main/java/com/gnn/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/gnn/eureka/EurekaApplication.java
@@ -2,8 +2,10 @@ package com.gnn.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer //这个注解让应用变成eureka服务器
 public class EurekaApplication {
 
     public static void main(String[] args) {
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 springcloud 广告系统
+
+eureka 端口：8000
